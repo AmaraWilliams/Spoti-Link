2a9cbf73e613c5acb65ed122abbacb6ffbc68cc1
AmaraWilliams <amaraajw@gmail.com>
2025-01-28 16:42:47 -0500

    Update error message for authentication and add login link

--- app/src/main/java/com/example/spotilink/Register.java
+++ app/src/main/java/com/example/spotilink/Register.java
@@ -1,8 +1,12 @@
 package com.example.spotilink;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +20,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,31 +33,55 @@ public class Register extends AppCompatActivity {
     private FirebaseAuth mAuth;
     TextInputEditText emailInput, passwordInput;
     Button registerBtn;
+
+    TextView login_now;
+
+    TextView email_textView, password_textView;
+    TextInputLayout emailLayout, passwordLayout;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+        //firebase authentication manager
+        mAuth = FirebaseAuth.getInstance();
+
+        //UI objects
         emailInput = findViewById(R.id.email);
         passwordInput = findViewById(R.id.password);
-        registerBtn = findViewById(R.id.register_btn);
 
-        mAuth = FirebaseAuth.getInstance();
+        emailLayout = findViewById(R.id.email_layout);
+        passwordLayout = findViewById(R.id.password_layout);
+
+        registerBtn = findViewById(R.id.register_btn);
+        progressBar = findViewById(R.id.progressBar);
+        login_now = findViewById(R.id.loginNow);
+
+
+        login_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email, password = null;
-                try {
-                    email = Objects.requireNonNull(emailInput.getText()).toString();
-                } catch (NullPointerException e) {
-                    emailInput.setError("email cannot be empty");
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(emailInput.getText());
+                password = String.valueOf(passwordInput.getText());
+                if (TextUtils.isEmpty(email)) {
+                    emailLayout.setError("email cannot be empty");
                     return;
+
                 }
-                try {
-                    password = Objects.requireNonNull(passwordInput.getText()).toString();
-                } catch (NullPointerException e) {
-                    passwordInput.setError("password cannot be empty");
+                if (TextUtils.isEmpty(password)) {
+                    passwordLayout.setError("password cannot be empty");
                     return;
                 }
 
@@ -62,6 +91,7 @@ public class Register extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     //FirebaseUser user = mAuth.getCurrentUser();
