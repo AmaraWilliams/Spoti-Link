ecd3d72635afe520bdeb218a87d2e2469c2dff08
AmaraWilliams <amaraajw@gmail.com>
2025-01-28 16:43:34 -0500

    add authentication and link to register

--- app/src/main/java/com/example/spotilink/Login.java
+++ app/src/main/java/com/example/spotilink/Login.java
@@ -1,27 +1,105 @@
 package com.example.spotilink;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
     TextInputEditText emailInput, passwordInput;
     Button loginBtn;
+    ProgressBar progressBar;
+    TextView register_now;
+
+    TextInputLayout emailLayout, passwordLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        //firebase authentication manager
+        mAuth = FirebaseAuth.getInstance();
+
         emailInput = findViewById(R.id.email);
         passwordInput = findViewById(R.id.password);
+
+        emailLayout = findViewById(R.id.email_layout);
+        passwordLayout = findViewById(R.id.password_layout);
+
         loginBtn = findViewById(R.id.login_btn);
+        progressBar = findViewById(R.id.progressBar);
+        register_now = findViewById(R.id.registerNow);
+
+        register_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password = null;
+                email = String.valueOf(emailInput.getText());
+                password = String.valueOf(passwordInput.getText());
+                if (TextUtils.isEmpty(email)) {
+                    emailLayout.setError("email cannot be empty");
+                    return;
+
+                }
+                if (TextUtils.isEmpty(password)) {
+                    passwordLayout.setError("password cannot be empty");
+                    return;
+                }
+
+                //validPassword(password);
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    //FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Login.this, "Account Created.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
