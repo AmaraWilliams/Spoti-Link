50325baad24f679393c5e3d1127c245a6e4c8a20
AmaraWilliams <amaraajw@gmail.com>
2025-01-28 19:09:30 -0500

    add clock

--- app/src/main/java/com/example/spotilink/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/spotilink/ui/home/HomeFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextClock;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.spotilink.R;
 import com.example.spotilink.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    TextClock clock;
 
 private FragmentHomeBinding binding;
 
@@ -19,11 +24,11 @@ private FragmentHomeBinding binding;
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        clock = root.findViewById(R.id.clock);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
